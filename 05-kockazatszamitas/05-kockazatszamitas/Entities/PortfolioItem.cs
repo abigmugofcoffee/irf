d9b516f6b7cab812d0d818a718c10d52266f3781
Zsófia Erdélyi <zsofia.erdelyi2@stud.uni-corvinus.hu>
2021-10-22 22:50:16 +0200

    Ticks (adatok betoltese) es Portfolio (3 portfolio) listak

---
+++ 05-kockazatszamitas/05-kockazatszamitas/Entities/PortfolioItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_kockazatszamitas
+{
+    class PortfolioItem
+    {
+        public string Index { get; set; }
+        public int Volume { get; set; }
+
+    }
+}
