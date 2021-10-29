c1cc39993ea3bc4e1a99e9fba2f1e77d87f67b8d
Erdélyi Zsófia <zsofia.erdelyi2@stud.uni-corvinus.hu>
2021-10-29 10:29:37 +0200

    webhivas01

--- webszolgaltatasok_elerese/webszolgaltatasok_elerese/Form1.cs
+++ webszolgaltatasok_elerese/webszolgaltatasok_elerese/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using webszolgaltatasok_elerese.MnbReference;
 
 namespace webszolgaltatasok_elerese
 {
     public partial class Form1 : Form
     {
+        List<MNBArfolyamServiceSoap> mnbservic = new List<MNBArfolyamServiceSoap>();
+        GetExchangeRatesRequestBody reques = new GetExchangeRatesRequestBody();
+        
+
         public Form1()
         {
             InitializeComponent();
+            Webhivas();
         }
+
+        void Webhivas()
+        {
+            var mnbservice = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            { currencyNames = "EUR", startDate = "2020-01-01", endDate = "2020-06-30" };
+
+            var response =mnbservice.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult; ;
+            
+        }
+
     }
 }
