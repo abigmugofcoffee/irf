f126439389615714a72e7bc500d4b40acb0431d8
Erdélyi Zsófia <zsofia.erdelyi2@stud.uni-corvinus.hu>
2021-10-29 23:36:25 +0200

    xml hivasa es beolvasasa

--- webszolgaltatasok_elerese/webszolgaltatasok_elerese/Form1.cs
+++ webszolgaltatasok_elerese/webszolgaltatasok_elerese/Form1.cs
@@ -7,33 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using webszolgaltatasok_elerese.MnbReference;
 
 namespace webszolgaltatasok_elerese
 {
     public partial class Form1 : Form
     {
-        List<MNBArfolyamServiceSoap> mnbservic = new List<MNBArfolyamServiceSoap>();
-        GetExchangeRatesRequestBody reques = new GetExchangeRatesRequestBody();
-        
+        BindingList<RateData> Rates = new BindingList<RateData>();
 
         public Form1()
         {
             InitializeComponent();
-            Webhivas();
+
+            var result = Webhivas();
+            Beolvasas(result);
+
+            dataGridView1.DataSource = Rates;
         }
 
-        void Webhivas()
+        private string Webhivas()
         {
             var mnbservice = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
             { currencyNames = "EUR", startDate = "2020-01-01", endDate = "2020-06-30" };
 
-            var response =mnbservice.GetExchangeRates(request);
+            var response = mnbservice.GetExchangeRates(request);
 
-            var result = response.GetExchangeRatesResult; ;
+            var result = response.GetExchangeRatesResult;
+            return result;
             
+        }
+
+        void Beolvasas(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                RateData rate = new RateData();
+                Rates.Add(rate);
+
+
+                rate.Date = Convert.ToDateTime(element.GetAttribute("date"));
+                
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = Convert.ToDecimal(childElement.GetAttribute("unit"));
+
+                if (unit != 0)
+                {
+                    rate.Value = Convert.ToDecimal(childElement.InnerText) / unit;
+                }
+                else rate.Value = 0;
+            }
+
         }
 
     }
