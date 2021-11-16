34b8c128bf70e40f78c6e47bbaa9721d6ace2d2a
Erdélyi Zsófia <zsofia.erdelyi2@stud.uni-corvinus.hu>
2021-11-16 21:10:48 +0100

    hibajavítások

--- webszolgaltatasok_elerese/webszolgaltatasok_elerese/Form1.cs
+++ webszolgaltatasok_elerese/webszolgaltatasok_elerese/Form1.cs
@@ -16,25 +16,46 @@ namespace webszolgaltatasok_elerese
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        //BindingList<string> Currencies = new BindingList<string>();
 
         public Form1()
         {
             InitializeComponent();
 
-            var result = Webhivas();
-            Beolvasas(result);
-            Diagram();
+            //comboBox1.DataSource = Currencies;
+
+            //var mnbService = new MNBArfolyamServiceSoapClient();
+            //var requestcurr = new GetCurrenciesRequestBody();
+            //var responsecurr = mnbService.GetCurrencies(requestcurr);
+            //var resultcurr = responsecurr.GetCurrenciesResult;
+            //var xml = new XmlDocument();
+            //xml.LoadXml(resultcurr);
+
+            RefreshData();
 
             dataGridView1.DataSource = Rates;
             chartRateData.DataSource = Rates;
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+
+            var result = Webhivas();
+            Beolvasas(result);
+            Diagram();
         }
 
         private string Webhivas()
         {
             var mnbservice = new MNBArfolyamServiceSoapClient();
 
+            string startdate = dateTimePicker1.Value.ToString();
+            string enddate = dateTimePicker2.Value.ToString();
+            string curr = comboBox1.SelectedItem.ToString();
+
             var request = new GetExchangeRatesRequestBody()
-            { currencyNames = "EUR", startDate = "2020-01-01", endDate = "2020-06-30" };
+            { currencyNames = curr, startDate = startdate, endDate = enddate};
 
             var response = mnbservice.GetExchangeRates(request);
 
@@ -71,6 +92,8 @@ namespace webszolgaltatasok_elerese
         }
         void Diagram()
         {
+            chartRateData.DataSource = Rates;
+
             var series = chartRateData.Series[0];
             series.ChartType = SeriesChartType.Line;
 
@@ -78,15 +101,28 @@ namespace webszolgaltatasok_elerese
             series.YValueMembers = "value";
 
             var legend = chartRateData.Legends[0];
-            //legend.Enabeled = false;
             var chartArea = chartRateData.ChartAreas[0];
 
             series.BorderWidth = 2;
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
-
+            legend.Enabled = false;
         }
 
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
     }
 }
