76296dc1d7c7d4be949df7de48fd427168122667
Erdélyi Zsófia <zsofia.erdelyi2@stud.uni-corvinus.hu>
2021-11-19 00:59:34 +0100

    trying with balls and cars

--- 07_fejlesztesi_mintak/07_fejlesztesi_mintak/Form1.cs
+++ 07_fejlesztesi_mintak/07_fejlesztesi_mintak/Form1.cs
@@ -12,9 +12,78 @@ namespace _07_fejlesztesi_mintak
 {
     public partial class Form1 : Form
     {
+        private List<Toy> _toys = new List<Toy>();
+        private Toy _nextToy;
+        private IToyFactory _factory;
+        public IToyFactory Factory
+        {
+            get { return _factory; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            if (Factory == null)
+                Factory = new BallFactory();
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            int position = 0;
+            foreach (var toy in _toys)
+            {
+                toy.MoveToy();
+                if (position < toy.Left) position = toy.Left;
+            }
+            if (position > 1000)
+            {
+                var oldestToy = _toys[0];
+                mainPanel.Controls.Remove(oldestToy);
+                _toys.Remove(oldestToy);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+            _nextToy = new Car();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
         }
     }
 }
